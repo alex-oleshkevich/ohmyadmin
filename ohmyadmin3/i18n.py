96f74af9848a87341f2faedab44b548183fa9c37
alex.oleshkevich <alex.oleshkevich@gmail.com>
2022-09-15 21:43:37 +0300

    remove old versions

--- ohmyadmin3/i18n.py
+++
@@ -1,2 +0,0 @@
-def _(message: str) -> str:
-    return message
--- ohmyadmin3/validators.py
+++
@@ -1,7 +0,0 @@
-import typing
-import wtforms
-
-
-class Validator(typing.Protocol):
-    async def __call__(self, form: wtforms.Form, field: wtforms.Field) -> None:
-        ...
