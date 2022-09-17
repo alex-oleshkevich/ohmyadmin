a728acd2c5e8d33f498a8178b6066cececa55676
alex.oleshkevich <alex.oleshkevich@gmail.com>
2022-09-17 12:06:15 +0300

    rename export -> duplicate

--- examples/admin/users.py
+++ examples/admin/users.py
@@ -4,24 +4,14 @@ from starlette.requests import Request
 from examples.models import User
 from ohmyadmin.actions import ActionResponse
 from ohmyadmin.batch_actions import BatchAction
-from ohmyadmin.forms import CheckboxField, EmailField, FileField, Form, HiddenField, SelectField, TextField
+from ohmyadmin.forms import CheckboxField, EmailField, FileField, Form, HiddenField, IntegerField, TextField
 from ohmyadmin.resources import PkType, Resource
 from ohmyadmin.tables import BoolColumn, Column, ImageColumn
 
 
-class ExportAction(BatchAction):
-    label = 'Export'
-
+class DuplicateAction(BatchAction):
     class ActionForm(Form):
-        columns = SelectField(
-            required=True,
-            choices=[
-                ('id', 'ID'),
-                ('first_name', 'First name'),
-                ('last_name', 'Last name'),
-                ('email', 'Email'),
-            ],
-        )
+        count = IntegerField(min_value=1, default=1)
 
     async def apply(self, request: Request, ids: list[PkType], form: Form) -> ActionResponse:
         return self.respond().redirect_to_resource(UserResource).with_success('User has been scheduled for export.')
@@ -43,7 +33,7 @@ class UserResource(Resource):
     entity_class = User
     form_class = EditForm
     queryset = sa.select(entity_class).order_by(User.id)
-    batch_actions = (ExportAction(),)
+    batch_actions = (DuplicateAction(),)
     table_actions = []
     table_columns = [
         Column('id', label='ID'),
