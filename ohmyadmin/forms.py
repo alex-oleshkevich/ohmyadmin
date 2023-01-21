bdc05cf42772ed64ce4ce0174554a46a41d097e5
alex.oleshkevich <alex.oleshkevich@gmail.com>
2023-01-21 15:46:10 +0300

    support async validators

--- ohmyadmin/forms.py
+++ ohmyadmin/forms.py
@@ -1,6 +1,8 @@
 import abc
+import inspect
 import typing
 import wtforms
+from starlette.concurrency import run_in_threadpool
 from starlette.requests import Request
 
 _F = typing.TypeVar('_F', bound=wtforms.Form)
@@ -11,8 +13,34 @@ async def create_form(request: Request, form_class: type[_F], obj: typing.Any | 
     return form_class(form_data, obj=obj)
 
 
+def iterate_form_fields(form: typing.Iterable[wtforms.Field]) -> typing.Generator[wtforms.Field, None, None]:
+    for field in form:
+        if isinstance(field, (wtforms.FieldList, wtforms.FormField)):
+            yield from iterate_form_fields(form)
+        else:
+            yield field
+
+
 async def validate_form(form: wtforms.Form) -> bool:
-    return form.validate()
+    """
+    Perform form validation.
+
+    This function does not call Form.validate or Field.validate, instead it
+    implements own logic that supports async validators.
+    """
+    is_valid = True
+    for field in iterate_form_fields(form):
+        for validator in field.validators:
+            try:
+                if inspect.iscoroutinefunction(validator):
+                    await validator(form, field)
+                else:
+                    await run_in_threadpool(validator, form, field)
+            except wtforms.ValidationError as ex:
+                field.errors = list(field.errors)
+                field.errors.extend(ex.args)
+                is_valid = False
+    return is_valid
 
 
 async def validate_on_submit(request: Request, form: wtforms.Form) -> bool:
